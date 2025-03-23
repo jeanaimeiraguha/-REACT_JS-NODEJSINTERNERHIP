82d07defd315aeb8f666c7ac57532c4fbeee139d
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-23 12:39:32 +0200

    Fullstsck

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import {Browserrouter ,Routes, Route} from 'react-router-dom'
+const Home = () => {
+  return (
+    <Browserrouter>
+    <Routes>
+     <Router>
+
+          
+     </Router>
+    </Routes>
+    
+    </Browserrouter>
+  )
+}
+
+export default Home
