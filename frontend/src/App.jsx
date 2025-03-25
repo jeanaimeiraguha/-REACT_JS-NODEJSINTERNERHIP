ab7bc2e3dd19e3e8a4df77fd144fe579037245a4
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-25 13:17:36 +0200

    Nodejs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import User from './user';
-import CreateUser from './createUser'; // ✅ Capitalized component name
+import CreateUser from './createUser';
 
 const App = () => {
   return (
--- frontend/src/createUser.jsx
+++ frontend/src/createUser.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
-const createUser = () => {
-  const[username, setUsername]= useState('')
-  const[address, setAddress]= useState('')
-  const navigate=useNavigate();
-  //Passing data to backend 
-  const handleSubmit=(event)=>{
-event.preventDefault();
-axios.post('http://localhost:5000/user/create',{username,address})
- .then(res=>{
-     console.log(res);
-     navigate('/');
- }).catch(err=>{
-     console.log(err)
- }) 
-} 
+const CreateUser = () => {
+  const [username, setUsername] = useState('');
+  const [address, setAddress] = useState('');
+  const navigate = useNavigate();
+
+  // Passing data to backend
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:5000/user/create', { username, address })
+      .then(res => {
+        console.log(res);
+        navigate('/'); // Redirect to homepage
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   return (
     <div>
-     <form onSubmit={handleSubmit}>
-         <input type="text" placeholder=' username'  onChange={e=>setUsername(e.target.value)}/> <br /><br />
-         <input type="text"  placeholder=' address' onChange={e=>setAddress(e.target.value)}/>
-          <br /><br />
-          <button>Submit</button>
-     </form>
-      
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text" 
+          placeholder="Username" 
+          value={username} 
+          onChange={e => setUsername(e.target.value)} 
+        /> <br /><br />
+        <input 
+          type="text" 
+          placeholder="Address" 
+          value={address} 
+          onChange={e => setAddress(e.target.value)} 
+        />
+        <br /><br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default createUser
+export default CreateUser;
