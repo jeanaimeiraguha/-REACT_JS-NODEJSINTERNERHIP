b2ded9e79ccee210e4b1b0f7d6f567fdac02d09f
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-25 12:50:04 +0200

    Nodejs

--- frontend/src/createUser.jsx
+++ frontend/src/createUser.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const createUser = () => {
+  const[username, setUsername]= useState('')
+  const[address, setAddress]= useState('')
+  //Passing data to backend 
+  const handleSubmit=(event)=>{
+event.preventDefault();
+axios.post('localhost:5000/user')
+  } 
   return (
     <div>
+     <form onSubmit={handleSubmit}>
+         <input type="text" placeholder=' username'  onChange={e=>setUsername(e.target.value)}/> <br /><br />
+         <input type="text"  placeholder=' address' onChange={e=>setAddress(e.target.value)}/>
+          <br /><br />
+          <button>Submit</button>
+     </form>
       
     </div>
   )
