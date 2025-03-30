b0591003e268ba43849ec8415bff901b2d55a07f
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-03-30 20:57:54 +0200

    Backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import mysql from 'mysql';
-const app =expres();
+const app =express();
 const db =mysql.createConnection({
 host:'localhost',
 user:'root',
@@ -9,6 +9,14 @@ database:'tests'
 })
 app.get("/",(req,res)=>{
      res.json("Hello This is the backend")
+})
+app.get("/books",()=>{
+     const sql ="SELECT * FROM books";
+   db.query(sql,(err,data)=>{
+     if (err)   return res.status(400).json("Error occured in selection");
+     
+
+   })  
 })
 app.listen(3000,()=>{
 console.log('running');
