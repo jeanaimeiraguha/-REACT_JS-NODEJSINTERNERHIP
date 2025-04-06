04eb26b623c3c8d23a8fb219c4c61e269b377071
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-06 15:29:31 +0200

    Frotnend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import Home from './Home';
-import About from './About'; // ✅ Corrected import
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+// import Home from './Home'
+// import About from './About'
+// import Services from './Services'
+// const App = () => {
+//   return (
+//     <>
+//     <Router>
+//       <Routes>
+//         <Route path='/' element={<Home/>}/>
+//         <Route path='/about' element={<About/>}/>
+//         <Route path='/services' element={<Services/>}/>
+//       </Routes>
+//     </Router>
+//     </>
+//   )
+// }
 
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </Router>
-  );
-};
-
-export default App;
+// export default App
---
+++ frontend/src/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+      <h2 style={{color:"blue", fontFamily:"Arial" , textAlign:"center"}}>Our sevices coming soon</h2>
+    </div>
+  )
+}
+
+export default Services
