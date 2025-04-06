72cec6232477e2c32a42ad101f4fa923a8bc453b
jeanaime-iraguha <jeanaimeiraguha@gmail.com>
2025-04-06 19:22:33 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,24 @@ import mysql from 'mysql'
 const app = express()
 app.use(express.json())
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'demo'  
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "demo"  
 })
+
 db.connect(err => {
   if (err) {
     console.log(' DB connection error:', err)
     return
   }
   console.log('Connected to MySQL database "demo"')
-})
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to your Node.js !');
-})
+});
 
 app.listen(5000, () => {
   console.log('Server is running at http://localhost:5000');
-})
+});
